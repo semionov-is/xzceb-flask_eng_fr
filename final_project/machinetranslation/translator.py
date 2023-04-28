10eb20b2d363cee842ec8c631771e99cc67a88fb
semionov-is <some_emaill@domain.com>
2023-04-28 05:32:46 -0400

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,6 +1,5 @@
 ```py
-"""This is module translator to translate en-fr, fr-en
-"""
+"""This is module translator to translate en-fr, fr-en"""
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -8,7 +7,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 # os.environ OR os.env???
 apikey = os.environ['apikey']
 url = os.environ['url']
@@ -22,12 +20,16 @@ language_translator = LanguageTranslatorV3(
 language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
-    frenchText = language_translator.translate(
+    """This is function to translate en-fr"""
+    french_translate = language_translator.translate(
         text=englishText, model_id='en-fr').get_result()
+    frenchText = french_translate.get("translations")[0].get("translation")
     return frenchText
 
 def frenchToEnglish(frenchText):
-   englishText = language_translator.translate(
+    """This is function to translate fr-en"""
+    english_translate = language_translator.translate(
         text=frenchText, model_id='fr-en').get_result()
+    englishText = english_translate.get("translations")[0].get("translation")
     return englishText
 ```
