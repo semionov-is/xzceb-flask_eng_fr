2002c850f8a638bfcb3ae2c0f1660692521565b5
semionov-is <some_emaill@domain.com>
2023-04-28 06:24:55 -0400

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,12 +1,11 @@
 """This is module translator to translate en-fr, fr-en"""
-import json
+#import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# os.environ OR os.env???
 apikey = os.environ['apikey']
 url = os.environ['url']
 
@@ -15,22 +14,18 @@ language_translator = LanguageTranslatorV3(
     version='2023-04-28',
     authenticator=authenticator
 )
-
 language_translator.set_service_url(url)
 
-def englishToFrench(englishText):
+def english_to_french(english_text):
     """This is function to translate en-fr"""
     french_translate = language_translator.translate(
-        text=englishText, model_id='en-fr').get_result()
-    frenchText = french_translate.get("translations")[0].get("translation")
-    return frenchText
+        text=english_text, model_id='en-fr').get_result()
+    french_text = french_translate.get("translations")[0].get("translation")
+    return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
     """This is function to translate fr-en"""
     english_translate = language_translator.translate(
-        text=frenchText, model_id='fr-en').get_result()
-    englishText = english_translate.get("translations")[0].get("translation")
-    return englishText
-
-text_p = englishToFrench('mother')
-print(text_p)
+        text=french_text, model_id='fr-en').get_result()
+    english_text = english_translate.get("translations")[0].get("translation")
+    return english_text
